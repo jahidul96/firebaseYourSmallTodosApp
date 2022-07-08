c60e5d21b5677b04818336853e7817a0e80b5b4e
Jahidul Islam <97932459+jahidul96@users.noreply.github.com>
2022-07-08 15:28:27 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,6 @@ const firebaseConfig = {
     //  add  your api key don't use mine because it's just for demo
     
     
-    apiKey: "AIzaSyAev79MapED2-3uKB7psPZfd_n9sUnWNdA",
-  authDomain: "bankpage-demos.firebaseapp.com",
-  projectId: "bankpage-demos",
-  storageBucket: "bankpage-demos.appspot.com",
-  messagingSenderId: "827055662271",
-  appId: "1:827055662271:web:540d84fbdcc3cb8f31120a"
 };
 
 
